af86b0fffaca1c7cc8160127c5a7ada89059bdc3
qianmimi <60906597+qianmimi@users.noreply.github.com>
2021-07-28 20:47:12 -0400

    Update run_pd_rpc.py

--- run_pd_rpc.py
+++ run_pd_rpc.py
@@ -9,8 +9,20 @@ p4_pd.ipv4_lpm2_table_add_with_set_egr(
     p4_pd.ipv4_lpm2_match_spec_t(ipv4Addr_to_i32("10.0.2.10"),0x0800),
     p4_pd.set_egr_action_spec_t(140))
 
+
+# p0
+p4_pd.ipv4_lpm2_table_add_with_set_egr(
+    p4_pd.ipv4_lpm2_match_spec_t(ipv4Addr_to_i32("10.0.2.11"),0x0800),
+    p4_pd.set_egr_action_spec_t(140))
+
 p4_pd.ipv4_lpm2_table_add_with_set_egr(
     p4_pd.ipv4_lpm2_match_spec_t(ipv4Addr_to_i32("10.0.1.11"),0x0800),
     p4_pd.set_egr_action_spec_t(132))
+
+
+p4_pd.ipv4_lpm2_table_add_with_set_egr(
+    p4_pd.ipv4_lpm2_match_spec_t(ipv4Addr_to_i32("10.0.1.10"),0x0800),
+    p4_pd.set_egr_action_spec_t(132))
+
 
 conn_mgr.complete_operations()
